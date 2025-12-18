85bd1ac17ccd78c5c07c6fab81f3bf58c66328db
Parik-2006 <parikshithbb.cs25@rvce.edu.in>
2025-12-18 18:05:50 +0530

    UPDATED

--- TREES/pre_order.cpp
+++ TREES/pre_order.cpp
@@ -1,24 +1,31 @@
-// creating a binary tree
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node{
     int data;
     Node *left;
     Node *right;
 
-    // Constructor
-    Node(int val) {
-        data = val;
+    // constructor
+    Node(int data){
+        this->data = data;
         left = right = NULL;
     }
 };
 
-int main() {
+void preorder(Node *root){
+    if(root == NULL)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->right = new Node(5);
-
+    preorder(root);
     return 0;
 }
