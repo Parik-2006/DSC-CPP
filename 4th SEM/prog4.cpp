d68bcf257b165cdce63e76bbd5f35b329e45a576
Parik-2006 <parikshithbb.cs25@rvce.edu.in>
2026-04-14 19:27:16 +0530

    Add simple quick sort (pivot=start) as comment

--- 4th SEM/prog4.cpp
+++ 4th SEM/prog4.cpp
@@ -1,3 +1,33 @@
+/*
+SIMPLE QUICK SORT (Pivot = Starting Element)
+
+#include <iostream>
+using namespace std;
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[low];
+        int i = low + 1, j = high;
+        while (i <= j) {
+            while (i <= high && arr[i] <= pivot) i++;
+            while (arr[j] > pivot) j--;
+            if (i < j) swap(arr[i], arr[j]);
+        }
+        swap(arr[low], arr[j]);
+        quickSort(arr, low, j - 1);
+        quickSort(arr, j + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << "\n";
+    return 0;
+}
+*/
 // QUICK SORT
 #include <iostream>
 #include <vector>
